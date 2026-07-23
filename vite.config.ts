c7d5d2c30a3aa9ef0ac3630d4ef57b8ee33558a2
raadkalil <msharbashraad@gmail.com>
2026-07-23 16:40:10 +0300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Cut-Pan-Zoom/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
